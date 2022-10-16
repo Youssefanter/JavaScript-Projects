6e584c0557a40e9cd3d0745d3cda94d875da8d1a
Youssefanter <yooiialy@gmail.com>
2022-10-16 23:37:13 +0200

    Done With Structure and Styling

--- Color Flipper/js/simple.js
+++
@@ -1,9 +0,0 @@
-let colorsArr = ["#F1f5f8", "Green", "Rgba(133,122,200)", "Red"];
-let btn = document.getElementById("btn");
-let mainPage = document.querySelector("main");
-let colorText = document.getElementById("color");
-btn.addEventListener("click", () => {
-  let randomColor = Math.floor(Math.random() * colorsArr.length);
-  mainPage.style.backgroundColor = `${colorsArr[randomColor]}`;
-  colorText.innerText = `${colorsArr[randomColor]}`;
-});
